8735aeaa1187128ee4742dc6926154d2dc2e9665
sadaqat <sadaqatdatusin@gmail.com>
2021-02-24 23:24:40 +0500

    save

---
+++ lib/views/pages/account/update_profile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UpdateProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [Text('Sadaqat hussain')],
+      ),
+    );
+  }
+}
